e128cb3ed3b07fabce68ba33931260546b4c255c
Çağlar <abc@example.com>
2022-09-05 20:05:35 +0300

    mer 1

--- src/_06_07/_01_.java
+++ src/_06_07/_01_.java
@@ -3,9 +3,21 @@ package _06_07;
 public class _01_ {
     public static void main(String[] args) {
 
+        System.out.println("merhaba1");
+        System.out.println("Merhaba Dünya");
+
+
+        System.out.println("mer 1");
 
 
 
-        
+
+
+
+
+
+
+
+
     }
 }
